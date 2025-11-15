84ae3db99eb247699a28a5d8fa0f2033ac2a0071
jarabum <101054479+jarabum@users.noreply.github.com>
2025-11-15 16:21:04 +0100

    Added input box for setting custom wallpaper
    Added input box for setting custom wallpaper but its without the button to set the wallpaper for now

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ wallpaper1iconpath = os.path.abspath("images/icons/wallpaper1icon.png")
 wallpaper2iconpath = os.path.abspath("images/icons/wallpaper2icon.png")
 wallpaper3iconpath = os.path.abspath("images/icons/wallpaper3icon.png")
 
+pathtocustomwallpaper = "images/wallpapers/testpozadi2.jpg"
+customwallpaper = os.path.abspath(pathtocustomwallpaper)
+
 def change_wallpaper(image_path):
     # Constants for setting the wallpaper
     SPI_SETDESKWALLPAPER = 20
@@ -49,6 +52,15 @@ wallpaper2b.place(x=150,y=100) # Create button for wallpaper 2
 wallpaper3icon = PhotoImage(file=wallpaper3iconpath)
 wallpaper3b = Button(window, text="wallpaper3", image=wallpaper3icon, command=lambda:change_wallpaper(wallpaper3))
 wallpaper3b.place(x=275,y=100) # Create button for wallpaper 3
+
+customtext = Label(window, text="Set custom wallpaper", font=("Arial", 15, "bold"), fg="black", bg="white")
+customtext.place(x=100,y=225) # Add text "Set custom wallpaper"
+
+pathtext = Label(window, text="Path to wallpaper", font=("Arial", 10), fg="black", bg="white")
+pathtext.place(x=130,y=260)
+
+wallpaperinput = Entry(window, textvariable=pathtocustomwallpaper)
+wallpaperinput.place(x=130,y=280)
 
 credit = Label(window, text="by:jarabum", width=8, height=int(0.5), fg="black", bg="white")
 credit.place(x=0,y=380) # Add text for my credit :)
